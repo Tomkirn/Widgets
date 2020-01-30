e0b7e5e5266980998ce6ff30614caeeed339a104
Thomas <Tomkirn@users.noreply.github.com>
2020-01-30 21:27:11 +0100

    new version with counter

--- Widget.py
+++ Widget.py
@@ -2,32 +2,25 @@
 
 '''
 Dieses Widget stellt einen simplen Launcher dar
-und zeigt die interne IP und SSID
+und zeigt die interne und externe IP
 '''
 
 import appex
 import ui
 import socket
-from objc_util import *
+import datetime
 
-NSBundle.bundleWithPath_('/System/Library/PrivateFrameworks/SoftwareUpdateServices.framework').load()
-
-cNetwork = ['None','WiFi','Cellular']
-
-def currentNetworkType():
-	netMon=ObjCClass('SUNetworkMonitor').sharedInstance()
-	return netMon.currentNetworkType()
-	
-def get_ssid():
-	CNCopyCurrentNetworkInfo = c.CNCopyCurrentNetworkInfo
-	CNCopyCurrentNetworkInfo.restype = c_void_p
-	CNCopyCurrentNetworkInfo.argtypes = [c_void_p]
-	info = ObjCInstance(CNCopyCurrentNetworkInfo(ns('en0')))
-	try:
-		ssid = str(info['SSID'])
-	except:
-		ssid = 'kein WLAN'
-	return ssid
+def time_to_juist():
+	delta = (datetime.date(2020, 5, 30) - datetime.date.today())
+	tage = delta.days
+	tagetxt = str(tage)+' Tage'
+	if tage > 100:
+		symbol = '☹️'
+	elif tage > 50:
+		symbol = '😕'
+	else:
+		symbol = '🙂'
+	return tagetxt, symbol
 
 def get_local_ip():
 	s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -48,37 +41,31 @@ def button_action(sender):
 		import webbrowser
 		webbrowser.open(sender.name)
 
+vfont = ('<system>', 14)
+efont = ('<system>', 36)
 view = ui.View()
 
-label1 = ui.Label(font=('Menlo', 18), text='IP: '+get_local_ip())
-'''if currentNetworkType() == 1:
-	label2 = ui.Label(font=('Menlo', 18), text='SSID: '+get_ssid())
-else:
-	label2 = ui.Label(font=('Menlo', 18), text='No WiFi')'''
-label3 = ui.Label(font=('Menlo', 18), text='eIP: '+get_global_ip())
+label1 = ui.Label(font=vfont, text='IP: '+get_local_ip())
+label2 = ui.Label(font=vfont, text='eIP: '+get_global_ip())
+label3 = ui.Label(font=vfont, text='Juist: '+time_to_juist()[0])
+label4 = ui.Label(font=efont, text=time_to_juist()[1])
 
-button1 = ui.Button(title=' ⚓️ NavTex ', border_width=0, action=button_action, name='pythonista3://NavTest.py?action=run&root=icloud', corner_radius=9, bg_color='#00ffde')
-button2 = ui.Button(title=' 💻 Pythonista ', border_width=0, action=button_action, name='pythonista3://', corner_radius=9, bg_color='#b1ff00')
-button3 = ui.Button(title=' 🌦 DWD Wetter ', border_width=0, action=button_action, name='pythonista3://DWDWetter.py?action=run&root=icloud', corner_radius=9, bg_color='#00ffde')
-button4 = ui.Button(title=' 🤡 ', border_width=0, action=button_action, name='pythonista3://', corner_radius=9, bg_color='#b1ff00')
+button1 = ui.Button(title=' ⚓️ NavTex ', font=vfont, border_width=0, action=button_action, name='pythonista3://NavTest.py?action=run&root=icloud', corner_radius=9, bg_color='#00ffde')
+button2 = ui.Button(title=' 🌦 DWD Wetter ', font=vfont, border_width=0, action=button_action, name='pythonista3://DWDWetter.py?action=run', corner_radius=9, bg_color='#00ffde')
 
 view.add_subview(label1)
-#view.add_subview(label2)
+view.add_subview(label2)
 view.add_subview(label3)
+view.add_subview(label4)
 view.add_subview(button1)
 view.add_subview(button2)
-view.add_subview(button3)
-view.add_subview(button4)
-#view.height = 200
 
 appex.set_widget_view(view)
 
 label1.frame = (150, 10, 220, 35)
-#label2.frame = (150, 70, 220, 35)
-label3.frame = (150, 40, 220, 35)
+label2.frame = (150, 35, 220, 35)
+label3.frame = (150, 60, 220, 35)
+label4.frame = (view.width-60,60,50,35)
 
 button1.frame = (10, 15, 120, 35)
 button2.frame = (10, 60, 120, 35)
-button3.frame = (view.width-130, 15, 120, 35)
-button4.frame = (view.width-130, 60, 120, 35)
-
